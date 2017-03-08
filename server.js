6f6fa76f727078c583b1432b566499860642594b
vedler <vedler@users.noreply.github.com>
2017-03-08 10:37:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
+    res.end('Hello, world! <br\> UT Discord Admin Tool Test Site');
     
 }).listen(process.env.PORT || 8080);
