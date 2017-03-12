919f66736abc69d256d2d7a994e967438d771e1f
ANDRI-ARVUTI\Andri <andparv@gmail.com>
2017-03-12 21:43:36 +0200

    pmst vist esialgsed 2,5lehte vms

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function (req, res) {
-    
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world! <br\> UT Discord Admin Tool Test Siter asdasd');
-    
-}).listen(process.env.PORT || 8080);
+app.use(express.static('public'));
+app.get('/', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.htm" );
+})
+
+app.get('/login', function (req, res) {
+   res.sendFile( __dirname + "/" + "login.htm" );
+})
+
+app.get('/register', function (req, res) {
+   res.sendFile( __dirname + "/" + "register.htm" );
+})
+
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+var server = app.listen(process.env.PORT, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+
+})
+
