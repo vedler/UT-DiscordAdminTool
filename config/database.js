460b98bbf33db9ae666dae03b37b43c905d123a5
VEDLER-PC\vedler <kristjan.vedler@eesti.ee>
2017-03-26 18:38:19 +0300

    Initial user system using Passport and MySQL

---
+++ config/database.js
@@ -0,0 +1,13 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'd42704.mysql.zonevs.eu',
+        'user': 'd42704_discord',
+        'password': 'WtVb57'
+    },
+    'database': 'd42704_discordadm',
+    'users_table': 'users'
+};
+
+// SQL: d42704_discord - WtVb57
+// Server: d42704_discordadm
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-var express = require('express');
+/*var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
+
 app.get('/', function (req, res) {
    res.sendFile( __dirname + "/" + "index.htm" );
 })
@@ -24,10 +25,65 @@ app.get('/process_get', function (req, res) {
    res.end(JSON.stringify(response));
 })
 
+// SQL: d42704_discord - WtVb57
+// Server: d42704_discordadm
+
 var server = app.listen(process.env.PORT, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
 
-})
+})*/
+
+// server.js
+
+// set up ======================================================================
+// get all the tools we need
+var express = require('express');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var app = express();
+var port = process.env.PORT || 8080;
+
+var passport = require('passport');
+var flash = require('connect-flash');
+
+// configuration ===============================================================
+// connect to our database
+
+require('./config/passport')(passport); // pass passport for configuration
+
+
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({
+    secret: 'vidyapathaisalwaysrunning',
+    resave: true,
+    saveUninitialized: true
+})); // session secret
+
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
 
