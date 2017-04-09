5c394a90b16689b1b1c70d52d533a71c501bc0a3
VEDLER-PC\vedler <kristjan.vedler@eesti.ee>
2017-04-09 18:56:34 +0300

    Initial commit with sample parts around our new front end with Twitter's bootstrap

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 // setup
 
 // dependencies
+var path = require('path');
 var express = require('express');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
@@ -23,6 +24,8 @@ require('./config/passport')(passport); // pass passport for configuration
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
+
+app.use('/static', express.static(path.join(__dirname, 'static')))
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -64,4 +67,3 @@ require('./app/routes.js')(app, passport); // load our routes and pass in our ap
 // start the app
 app.listen(port);
 console.log('The magic happens on port ' + port);
-
