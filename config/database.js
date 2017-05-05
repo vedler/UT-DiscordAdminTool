f18adfc342f9811eb403acc68e5c5c77e7a00513
SALUS-PC\Salus <martin.salus@gmail.com>
2017-05-05 14:12:42 +0300

    account login and mongodb

--- config/database.js
+++ config/database.js
@@ -1,6 +1,17 @@
 // config/database.js
 // Dev database, whitelisted
+
+//mongodb
+
 module.exports = {
+
+	'url' : 'mongodb://127.0.0.1:27017/discordadm' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
+
+
+
+/*module.exports = {
     'connection': {
         'host': 'd42704.mysql.zonevs.eu',
         'user': 'd42704_discord',
@@ -11,7 +22,7 @@ module.exports = {
     },
     'database': 'd42704_discordadm',
     'users_table': 'users'
-};
+};*/
 
 // SQL: d42704_discord - WtVb57
 // Server: d42704_discordadm
